86c6e1b006a4431f86c9e570363a01626b104497
FcoArjona <fcoarjonaca@gmail.com>
2025-06-25 18:58:28 -0600

    New try

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# project7_repo2
-Reintento proyecto 7
+# project7_repo
+Repositorio del proyecto del Sprint 7, Análsisi de datos.
+Vehiculos para ti!
+La app muestra en primera instanacia, datos organizados en un grafico de barras y como opcion interactiva un grafico de dispersión, donde el usuario puede ver y de manera general los costos de de acuerdo a los tipos y condiciones de las opciones de vehiculos que existen.
+Tenemos un boton para mosttrar un histograma que muestra el tipo de carro y el rocorrido que tiene, si el usuario le intweresa el kilometraje.
+Finalmente se puede observar las opciones en base a los modelos, su precio y condition con el botono de mostrar grafico de dispersion.
---
+++ app.py
@@ -0,0 +1,25 @@
+# import packages
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+s
+
+# Read de csv file
+vehicles = pd.read_csv("/Users/fcoar/project7_repo/vehicles_us.csv")
+st.header("Vehicles for you") # Title of the app
+st.markdown("What are you looking for when youwant to buy a car?") # Subtitle of the app
+
+# Creating charts with Plotly
+hist_button = st.button("Show histogram")
+if hist_button:
+    st.write("Built and displayed a histogram")
+    st.markdown("***The type you like most, and how much they have run***")
+    fig = px.histogram(vehicles, title="Type vs Km", x="type", y="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button("Show scatter plot")
+if scatter_button:
+    st.write("Built and displayed a scatter plot")
+    fig = px.scatter(vehicles, x="model", y="price", color="condition")
+    event = st.plotly_chart(fig, key="iris", on_select="ignore", selection_mode="Iterable", use_container_width=False)
+    event.selection
